0d6cd76de362417033c26f74a13eae9410733376
Leya <leya.min@mail.ru>
2026-02-24 23:24:26 +0300

    the third version

--- src/store/use-counter-store.ts
+++ src/store/use-counter-store.ts
@@ -1,18 +1,31 @@
-import {create} from "zustand";
+import {create, type StateCreator} from "zustand";
 
-interface ICounterState {
-    count: number;
+interface IActions {
     increment: () => void;
     decrement: () => void;
 }
 
+interface IInitialState {
+    count: number;
+}
+
+interface ICounterState extends IInitialState, IActions {}
+
+
+const initialState: IInitialState = {
+    count: 0
+};
+
 //внутри криэйт - колбэк
-export const useCounterStore = create<ICounterState>()((set) => ({
-    count: 4,
+const сounterStore: StateCreator<ICounterState> = ((set) => ({
+    ...initialState,
     increment: () => set((state) => ({count: state.count + 1})),
     decrement:() => set((state) => ({count: state.count - 1}))
 }))
 
+const useCounterStore = create<ICounterState>()(сounterStore);
+
+// селекторы
 export const useCount = () => useCounterStore((state) => state.count);
 //export const incrementCount = () => useCounterStore((state) => state.increment);
 export const incrementCount = () => useCounterStore.getState().increment;
